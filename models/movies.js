8b22adb13708bd46cba92218dccfb3bb3f5b7b48
jeremykolker <123975631+jeremykolker@users.noreply.github.com>
2023-04-03 22:14:22 -0400

    Update movies.js

--- models/movies.js
+++ models/movies.js
@@ -5,12 +5,9 @@ const movieSchema = new mongoose.Schema({
     title: String,
     release_date: String,
     overview: String,
-<<<<<<< HEAD
     vote_average: Number,
     
-=======
-    vote_average: Number
->>>>>>> 2d4bdfa66ca65b97a2eac22b67101cd24e292ca3
+
 })
 
 const Movies = mongoose.model('Movies', movieSchema)
